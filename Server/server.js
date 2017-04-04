f726c788764d0750c7e266c761d845d1b49a2631
aamir <aamir.ramay1@gmail.com>
2017-04-04 21:40:54 +0500

    After Testing the get id

--- Server/server.js
+++ Server/server.js
@@ -62,8 +62,8 @@ app.get('/todos/:id', (req, res) => {
     }
 
     Todo.findById(id).then((todo) => {
-
-        if(!todo) {
+    
+       if(!todo) {
              return res.status(404).send();
         }
        res.send({todo}); // same res.send({todo: todo});
--- Server/tests/server.test.js
+++ Server/tests/server.test.js
@@ -4,12 +4,15 @@ const request = require('supertest');
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
+const {ObjectID} = require('mongodb');
 /*testing lifecycle method ...
 make sure db is empty */
 
 const todos = [{
+    _id: new ObjectID(),
     text: 'First test todo'
 }, {
+    _id: new ObjectID(),
     text: 'Second test todo'
 }];
 
@@ -31,6 +34,7 @@ describe('POST /todos ', () => {
             .post('/todos')
             .send({text}) // we send obj , this obj is gonna converted to JSON by supertest by default ...
             .expect(200) // assertion 
+            
             .expect((res) => { // body that comes has text property = that one we specify, and iz obj ... 
 
                 expect(res.body.text).toBe(text);
@@ -86,5 +90,33 @@ describe('POST /todos ', () => {
                 })
                 .end(done);
         });
+   });
+
+   describe('GET/ todos/:id', () => {
+       
+    it('should return todo doc', (done) => {
+        request(app)
+            .get(`/todos/${todos[0]._id.toHexString()}`) // obj id so need to convert into string 
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todo.text).toBe(todos[0].text);
+            })
+            .end(done);
+      });
+
+      it('should return 404 if todo not found', (done) => {
+        request(app)
+            .get(`/todos/${new ObjectID().toHexString()}`)
+            .expect(404)
+            .end(done);
+      });
+
+      it('should return 404 for non-object ids', (done) => {
+        request(app)
+            .get('/todos/123abc')
+            .expect(404)
+            .end(done);
+
+      });
    });
 });
